be42b3c3f2892c37ee064795b261ab0fa5d8f5b8
Elisei.Nicolae <43387542+EliseiNicolae@users.noreply.github.com>
2022-06-28 22:32:17 +0300

    Update sitemap.xml.tsx
    Update sitemap.xml.tsx
    
    Update sitemap.xml.tsx
    
    Update sitemap.xml.tsx
    
    Update sitemap.xml.tsx

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,45 +1,12 @@
 import React from "react";
-import matter from "gray-matter";
 
 class Sitemap extends React.Component {
   static async getInitialProps({ res }: any) {
-    if (typeof res.writeHead !== "function") {
-      return;
-    }
-
-    const fs = require("fs");
-    const files = fs?.readdirSync("posts");
-
-    const posts = files.map((fileName: string) => {
-      const slug = fileName.replace(".md", "");
-      const readFile = fs?.readFileSync(`posts/${fileName}`, "utf-8");
-      const { data: frontVariables } = matter(readFile);
-      return {
-        slug,
-        frontVariables,
-      };
-    });
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${posts
-          .map((post: any) => {
-            return `
-              <url>
-                <loc>https://code-bugs.com/post/${post.slug}</loc>
-                <lastmod>${
-                  post.frontVariables.date || new Date().toISOString()
-                }</lastmod>
-                <changefreq>monthly</changefreq>
-                <priority>1.0</priority>
-              </url>
-            `;
-          })
-          .join("")}
-      </urlset>
-  `;
-
     res.setHeader("Content-Type", "text/xml");
-    res.write(sitemap);
+    res.write(`<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ...
+    </urlset>`);
     res.end();
   }
 }
