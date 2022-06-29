27a8cb635953158ce45fef04e73f1d5992c67cef
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-06-29 13:18:37 +0300

    Created script that generates sitemap.xml (#1)
    * fix Cloudflare error build
    
    * fix.
    
    * fix
    
    * Update sitemap.xml.tsx
    
    Update sitemap.xml.tsx
    
    Update sitemap.xml.tsx
    
    Update sitemap.xml.tsx
    
    Update sitemap.xml.tsx
    
    * Revert "Update sitemap.xml.tsx"
    
    This reverts commit be42b3c3f2892c37ee064795b261ab0fa5d8f5b8.
    
    * create script to generate sitemapXML
    
    * fix error Cloudflare.

---
+++ public/js/sitemap-common.js
@@ -0,0 +1,37 @@
+function generateSitemap() {
+  const fs = require("fs");
+  const matter = require("gray-matter");
+  const files = fs.readdirSync("posts");
+
+  const posts = files.map(fileName => {
+    const slug = fileName.replace(".md", "");
+    const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8");
+    const { data: frontVariables } = matter(readFile);
+    return {
+      slug,
+      frontVariables,
+    };
+  });
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${posts
+          .map(post => {
+            return `
+              <url>
+                <loc>https://code-bugs.com/post/${post.slug}</loc>
+                <lastmod>${
+                  post.frontVariables.date || new Date().toISOString()
+                }</lastmod>
+                <changefreq>monthly</changefreq>
+                <priority>1.0</priority>
+              </url>
+            `;
+          })
+          .join("")}
+      </urlset>
+  `;
+
+  fs.writeFile("public/sitemap.xml", sitemap, () => {});
+}
+
+generateSitemap();
