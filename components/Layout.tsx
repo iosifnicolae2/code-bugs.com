6a1b45ec1e3f74d13f61a97ba14dd5825943b5c8
Elisei Nicolae <nicolae.elisei1999@gmail.com>
2022-06-29 16:07:12 +0300

    added google analytics & hotjar. (#2)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,34 @@
 import Link from "next/link";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function Layout({ seo, children }: any) {
   return (
     <>
+      <Script id="hotjar" strategy="afterInteractive">
+        {`
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3044114,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}
+      </Script>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-YVP4FD6510"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-YVP4FD6510');
+        `}
+      </Script>
       <Head>
         <title>{seo?.metaTitle}</title>
         <meta property="title" content={seo?.metaTitle} />
