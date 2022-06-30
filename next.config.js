eabdd322eb31091abc577566b321abec0588a51f
Elisei.Nicolae <nicolae.elisei1999@gmail.com>
2022-06-30 18:13:53 +0300

    Added redirects.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/setup-Clion",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/How-to-upload-your-project-to-Github",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
